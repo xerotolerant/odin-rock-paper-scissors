b77fe2cc3f2bf439c65a5edc5122a5b7bdb903d4
Kheenan Walkins <17203992+xerotolerant@users.noreply.github.com>
2024-10-04 08:22:17 -0400

    Write function to get computer choice

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log('Hello World');
+// Write a function that randomly returns 'rock', 'paper', or 'scissors'.
+function getComputerChoice() {
+    // Generate a random number between 1 and 3 inclusive
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    // Use the random number to select either 'rock', 'paper', or 'scissors' from a list of strings
+    // return the selected value
+    switch (randomNumber) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
+}
